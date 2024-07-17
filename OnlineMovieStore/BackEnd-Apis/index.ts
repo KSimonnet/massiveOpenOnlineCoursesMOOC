24689c783c67c0361fe5fb4d746a051b46d4bc46
KSimonnet <k.simonnet@gmail.com>
2024-07-17 12:59:23 +1000

    Added CRUD Update and Delete + uniformised the use of title as a variable + created transformValOf() to serialise

--- OnlineMovieStore/BackEnd-Apis/index.ts
+++ OnlineMovieStore/BackEnd-Apis/index.ts
@@ -4,6 +4,7 @@ import * as bodyParser from "body-parser";
 import * as cors from "cors";
 import * as sql from "msnodesqlv8";
 
+import { transformValOf } from "./modules/transform-values-of";
 import { toPascalCase } from "./utils/manip-str/index";
 
 app.use(cors());
@@ -93,7 +94,7 @@ app.post("/signup", (req: any, res: any) => {
           // insert new user
           const insert_query = `
             INSERT INTO Account (user_name, password_hash)
-            OUTPUT INSERTED.user_name, INSERTED.password_hash
+            OUTPUT INSERTED.*
             VALUES (?, ?);
           `; // OUTPUT clause captures inserted attributes to be displayed in the front-end
           sql.query(
@@ -155,14 +156,14 @@ app.get("/movies", ({ req, res }: any) => {
 app.post("/addmovie", async (req: any, res: any) => {
   try {
     // capitalize the first letter of each word
-    const pascal_case_req: Record<string, string | null> = Object.fromEntries(
-      Object.entries(req.body as Record<string, string | null>).map(
-        ([key, value]) => [key, value ? toPascalCase(value) : value],
-      ) as Array<[string, string | null]>,
+    const pascal_case_req: Record<string, string | null> = transformValOf(
+      req.body,
+      toPascalCase,
     ); // in TypeScript, `Record<string, string | null>` is a utility type that represents an object type with keys of type string and values that can either be of type string or null (https://www.typescriptlang.org/docs/handbook/utility-types.html#recordkeys-type)
     const { title, cast, category } = pascal_case_req;
 
     // in ER Diagram, "Allow NULL" is set to false for `title`. Ensure it's provided in the request body
+    // "" and '' (empty string) are falsy values (https://developer.mozilla.org/en-US/docs/Glossary/Truthy)
     if (!title) {
       return res.status(400).json({ error: "Movie title is required." });
     }
@@ -216,39 +217,35 @@ OUTPUT clause captures inserted attributes to be displayed in the front-end (htt
 app.get("/movie/:name", async (req: any, res: any) => {
   try {
     // type was already validated in Front-End; defined for clarity
-    const movie_name: string = req.params.name;
-    // whilst the Front-End checks that `name` is not empty, conditional chaining avoids breaking
-    if (movie_name?.length) {
-      // `movie_name` passed as a parameter to the query function,
-      // rather than directly by using `${movie_name}` to prevent SQL injection
-      const movie_query =
-        "SELECT title, cast, category FROM Movie WHERE title COLLATE SQL_Latin1_General_CP1_CI_AI LIKE ?"; // CI_AS (Case Insensitive, Accent Sensitive). A collation is a set of rules for comparing characters in a string.
-      sql.query(
-        connectionString,
-        movie_query,
-        [movie_name],
-        (err: any, rows: any) => {
-          if (err) {
-            // directly exposing error details from the server, especially those originating from a database,
-            // can provide attackers with insights into the backend structure, database schema, or even potential vulnerabilities.
-            // In addition, a generic error message is more user-friendly
-            console.error("SQL query error: ", err.message);
-            return res.status(500).json({ error: "Internal server error." });
-          }
-          if (rows && rows.length) {
-            console.log("Movie found: ", rows);
-            return res.status(200).json({
-              success: "We found the movie you wanted: ",
-              movie: rows,
-            });
-          } else {
-            return res.status(201).json({
-              conflict: "Sorry, this movie is not in store at the moment.",
-            });
-          }
-        },
-      );
+    const title: string = req.params.name;
+    // even though the Front-End checks that `name` is not empty, doube-check
+    if (!title) {
+      return res.status(400).json({ error: "Movie title is required." });
     }
+    // `title` passed as a parameter to the query function,
+    // rather than directly by using `${title}` to prevent SQL injection aka  parameterized query
+    const movie_query =
+      "SELECT title, cast, category FROM Movie WHERE title COLLATE SQL_Latin1_General_CP1_CI_AI LIKE ?"; // CI_AS (Case Insensitive, Accent Sensitive). A collation is a set of rules for comparing characters in a string.
+    sql.query(connectionString, movie_query, [title], (err: any, rows: any) => {
+      if (err) {
+        // directly exposing error details from the server, especially those originating from a database,
+        // can provide attackers with insights into the backend structure, database schema, or even potential vulnerabilities.
+        // In addition, a generic error message is more user-friendly
+        console.error("SQL query error: ", err.message);
+        return res.status(500).json({ error: "Internal server error." });
+      }
+      if (rows && rows.length) {
+        console.log("Movie found: ", rows);
+        return res.status(200).json({
+          success: "We found the movie you wanted: ",
+          movie: rows,
+        });
+      } else {
+        return res.status(201).json({
+          conflict: "Sorry, this movie is not in store at the moment.",
+        });
+      }
+    });
   } catch (err: any) {
     console.error("An error occurred: ", err.message);
     return res.status(501).json(err);
@@ -256,6 +253,112 @@ app.get("/movie/:name", async (req: any, res: any) => {
 });
 
 // CRUD - Update
+app.post("/updatemovie", async (req: any, res: any) => {
+  try {
+    // capitalize the first letter of each word
+    const pascal_case_req: Record<string, string | null> = transformValOf(
+      req.body,
+      toPascalCase,
+    ); // in TypeScript, `Record<string, string | null>` is a utility type that represents an object type with keys of type string and values that can either be of type string or null (https://www.typescriptlang.org/docs/handbook/utility-types.html#recordkeys-type)
+    const { title, cast, category } = pascal_case_req;
+
+    // in ER Diagram, "Allow NULL" is set to false for `title`. Ensure it's provided in the request body
+    // "" and '' (empty string) are falsy values (https://developer.mozilla.org/en-US/docs/Glossary/Truthy)
+    if (!title) {
+      return res.status(400).json({ error: "Movie title is required." });
+    }
+    // check if movie already exists
+    const query = `
+    IF EXISTS (SELECT 1 FROM Movie WHERE title = ?)
+    BEGIN
+    UPDATE Movie
+    SET title = ?, cast = ?, category = ?
+    OUTPUT INSERTED.*
+    WHERE title COLLATE SQL_Latin1_General_CP1_CI_AI LIKE ?
+    END
+  `;
+    sql.query(
+      connectionString,
+      query,
+      [title, title, cast, category, title],
+      (err: any, resultset: any) => {
+        if (err) {
+          /* directly exposing error details from the server, especially those originating from a database,
+          can provide attackers with insights into the backend structure, database schema, or even potential vulnerabilities.
+          In addition, a generic error message is more user-friendly */
+          console.error("SQL query error: ", err.message);
+          return res.status(500).json({ error: "Internal server error." });
+        }
+        if (resultset && resultset.length) {
+          console.log("Movie updated in DB: ", resultset);
+          return res
+            .status(200)
+            .json({ success: "Movie successfully updated.", movie: resultset });
+        } else {
+          // in case the movie doesn't exist, the `OUTPUT` doesn't get executed, ie. result set is null
+          return res.status(201).json({
+            conflict: "Sorry, this movie is not in store at the moment.",
+          });
+        }
+      },
+    );
+  } catch (err: any) {
+    console.error("An error occurred: ", err.message);
+    return res.status(501).json({ error: "Internal server error." });
+  }
+});
+
+// CRUD - Delete
+app.post("/deletemovie/:name", async (req: any, res: any) => {
+  try {
+    // type was already validated in Front-End; defined for clarity
+    const title: string = req.params.name;
+
+    // in ER Diagram, "Allow NULL" is set to false for `title`. Ensure it's provided in the request body
+    // even though the Front-End checks that `name` is not empty, double-check
+    // "" and '' (empty string) are falsy values (https://developer.mozilla.org/en-US/docs/Glossary/Truthy)
+    if (!title) {
+      return res.status(400).json({ error: "Movie title is required." });
+    }
+    // check if movie already exists
+    const query = `
+    IF EXISTS (SELECT 1 FROM Movie WHERE title = ?)
+    BEGIN
+    DELETE FROM Movie
+    OUTPUT DELETED.*
+    WHERE title COLLATE SQL_Latin1_General_CP1_CI_AI LIKE ?
+    END
+  `;
+    sql.query(
+      connectionString,
+      query,
+      [title, title],
+      (err: any, resultset: any) => {
+        if (err) {
+          /* directly exposing error details from the server, especially those originating from a database,
+          can provide attackers with insights into the backend structure, database schema, or even potential vulnerabilities.
+          In addition, a generic error message is more user-friendly */
+          console.error("SQL query error: ", err.message);
+          return res.status(500).json({ error: "Internal server error." });
+        }
+        if (resultset && resultset.length) {
+          console.log("Movie deleted in DB: ", resultset);
+          return res
+            .status(200)
+            .json({ success: "Movie successfully deleted.", movie: resultset });
+        } else {
+          // in case the movie doesn't exist, the `OUTPUT` doesn't get executed, ie. result set is null
+          return res.status(201).json({
+            conflict: "Sorry, this movie is not in store at the moment.",
+          });
+        }
+      },
+    );
+  } catch (err: any) {
+    console.error("An error occurred: ", err.message);
+    return res.status(501).json({ error: "Internal server error." });
+  }
+});
 
 // error handling
 // add movies to watchlist
