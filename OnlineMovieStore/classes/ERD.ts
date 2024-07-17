e9c7299c387ddfca1966a503c6548cd02930dace
KSimonnet <k.simonnet@gmail.com>
2024-07-17 10:02:21 +1000

    Moved Classes definition to top-level directory, as it's used in both FE and BE files

--- OnlineMovieStore/classes/ERD.ts
+++ OnlineMovieStore/classes/ERD.ts
@@ -1,14 +1,19 @@
 class Account {
   user_id: number;
   user_name: string;
-  passwrod_hash: string;
-  watchlists: Watchlist[]; // One-to-Many relationship. Array of instances of Watchlist
+  password_hash: string;
+  watchlist: Watchlist; // One-to-One relationship. ie. Each `Account` has exactly one `Watchlist`
 
-  constructor(user_id: number, user_name: string, passwrod_hash: string) {
+  constructor(
+    user_id: number,
+    user_name: string,
+    password_hash: string,
+    watchlist_id: number,
+  ) {
     this.user_id = user_id;
     this.user_name = user_name;
-    this.passwrod_hash = passwrod_hash;
-    this.watchlists = [];
+    this.password_hash = password_hash;
+    this.watchlist = new Watchlist(watchlist_id, user_id);
   }
 }
 
@@ -16,12 +21,12 @@ interface IAccount {
   user_id: number;
   user_name: string;
   passwrod_hash: string;
-  watchlists: Watchlist[];
+  watchlist: Watchlist;
 }
 
 class Watchlist {
   watchlist_id: number;
-  user_id: number; // Many-to-One relationship
+  user_id: number;
 
   constructor(watchlist_id: number, user_id: number) {
     this.watchlist_id = watchlist_id;
@@ -43,12 +48,16 @@ class Movie {
   }
 }
 
+// serves as an associative entity that represents a Many-to-Many relationship between `Watchlist` and `Movie`
 class Watchlist_Item {
-  watchlist_id: number;
-  movie_ids: Movie[]; // One-to-Many relationship. Array of instances of Movie
+  watchlist_id: number; // One-to-Many relationship (ie. An instance of `Watchlist` can contain multiple instances of `Watchlist_Item`.
+  // multiple `Watchlist_Item` instances can reference a single instance of `Watchlist`)
+  movie_id: number; // One-to-Many relationship (ie. An instance of `Movie` can be referenced by multiple instances of `Watchlist_Item`.
+  // multiple `Watchlist_Item` instances can reference a single instance of `Movie`
 
   constructor(watchlist_id: number, movie_id: number) {
-    this.watchlist_id = watchlist_id;
-    this.movie_ids = [];
+    // composite Key
+    this.watchlist_id = watchlist_id; // foreign key (FK)
+    this.movie_id = movie_id; // foreign key (FK)
   }
 }
